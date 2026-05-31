1bd61339e8818dc7429cded327466de17edf438a
pentakll4002 <dangthienan1207@gmail.com>
2026-05-31 23:43:04 +0700

    Fix: Add PascalCase property mapping/alias getters to DanhSachTiepNhanDTO to match frontend naming expectations

--- backend/src/main/java/com/healthclinics/dto/DanhSachTiepNhanDTO.java
+++ backend/src/main/java/com/healthclinics/dto/DanhSachTiepNhanDTO.java
@@ -25,4 +25,44 @@ public class DanhSachTiepNhanDTO {
     private String tenLeTanDuyet;
     private Boolean isDeleted;
     private String trangThaiTiepNhan;
+
+    @com.fasterxml.jackson.annotation.JsonProperty("ID_TiepNhan")
+    public Long getID_TiepNhan() {
+        return idTiepNhan;
+    }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("ID_BenhNhan")
+    public Long getID_BenhNhan() {
+        return idBenhNhan;
+    }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("HoTenBN")
+    public String getHoTenBN() {
+        return tenBenhNhan;
+    }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("NgayTN")
+    public LocalDateTime getNgayTNVal() {
+        return ngayTN;
+    }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("CaTN")
+    public String getCaTNVal() {
+        return caTN;
+    }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("ID_NhanVien")
+    public Long getID_NhanVien() {
+        return idNhanVien;
+    }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("ID_LeTanDuyet")
+    public Long getID_LeTanDuyet() {
+        return idLeTanDuyet;
+    }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("TrangThaiTiepNhan")
+    public String getTrangThaiTiepNhanVal() {
+        return trangThaiTiepNhan;
+    }
 }
